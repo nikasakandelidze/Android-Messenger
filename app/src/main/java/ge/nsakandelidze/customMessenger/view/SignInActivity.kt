0bed702133fda250eeca52f50c0d9b9a56512e24
sakana <nikasakana@gmail.com>
2021-07-07 22:44:52 +0400

    added sign up page

--- app/src/main/java/ge/nsakandelidze/customMessenger/view/SignInActivity.kt
+++ app/src/main/java/ge/nsakandelidze/customMessenger/view/SignInActivity.kt
@@ -1,21 +1,29 @@
 package ge.nsakandelidze.customMessenger.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatDelegate
 import ge.nsakandelidze.customMessenger.R
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setupVisualElements()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
         //below line is temporary
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        initializeListeners()
     }
 
-    private fun setupVisualElements() {
-        actionBar?.hide()
-        supportActionBar?.hide()
+    private fun initializeListeners() {
+        initializeSignUpButtonListener()
+    }
+
+    private fun initializeSignUpButtonListener() {
+        findViewById<Button>(R.id.sign_up_button).setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
