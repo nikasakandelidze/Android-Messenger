5b7f303cf771fb772d27cd87a5948f62fc20438e
sakana <nikasakana@gmail.com>
2021-07-08 00:45:19 +0400

    minor refactor

--- app/src/main/java/ge/nsakandelidze/customMessenger/view/signUp/SignUpActivity.kt
+++ app/src/main/java/ge/nsakandelidze/customMessenger/view/signUp/SignUpActivity.kt
@@ -4,21 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 import ge.nsakandelidze.customMessenger.R
 import ge.nsakandelidze.customMessenger.presenter.signUp.SignUpPresenter
 
 class SignUpActivity : AppCompatActivity(), ISignUpView {
     private lateinit var presenter: SignUpPresenter
+    private lateinit var usernameComponent: TextInputEditText
+    private lateinit var passwordComponent: TextInputEditText
+    private lateinit var professionComponent: TextInputEditText
+    private lateinit var signUpButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        initPresenter()
+        initializeState()
         initializeListeners()
     }
 
-    private fun initPresenter() {
+    private fun initializeState() {
         presenter = SignUpPresenter(this)
+        usernameComponent = findViewById(R.id.username)
+        passwordComponent = findViewById(R.id.password)
+        professionComponent = findViewById(R.id.what_i_do)
+        signUpButton = findViewById<Button>(R.id.sign_up_button)
     }
 
     private fun initializeListeners() {
@@ -26,8 +36,11 @@ class SignUpActivity : AppCompatActivity(), ISignUpView {
     }
 
     private fun initializeSignUpButtonListener() {
-        findViewById<Button>(R.id.sign_up_button).setOnClickListener {
-            presenter.signUpNewUser("", "", "")
+        signUpButton.setOnClickListener {
+            val username: String = usernameComponent.text.toString()
+            val password: String = passwordComponent.text.toString()
+            val profession: String = professionComponent.text.toString()
+            presenter.signUpNewUser(username, password, profession)
         }
     }
 
