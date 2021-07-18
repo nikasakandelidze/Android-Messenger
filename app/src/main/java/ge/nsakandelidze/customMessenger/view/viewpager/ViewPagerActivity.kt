b58ff69c47e38ca50f73efa298eb254a6d71a88e
rezimeshvelashvili <rmesh16@freeuni.edu.ge>
2021-07-18 13:39:04 +0400

    added viepager and fragments(fragmnets are not done yet)

---
+++ app/src/main/java/ge/nsakandelidze/customMessenger/view/viewpager/ViewPagerActivity.kt
@@ -0,0 +1,41 @@
+package ge.nsakandelidze.customMessenger.view.viewpager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import ge.nsakandelidze.customMessenger.R
+import ge.nsakandelidze.customMessenger.view.profile.HomePage
+import ge.nsakandelidze.customMessenger.view.profile.ProfilePage
+
+
+class ViewPagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if(supportActionBar!=null){
+            this.supportActionBar?.hide()
+        }
+        setContentView(R.layout.view_pager)
+        val info : List<Fragment> = listOf(ProfilePage(), HomePage())
+
+        val adapter = ViewPagerAdapter(info, this)
+
+
+        findViewById<ViewPager2>(R.id.pager).adapter = adapter
+
+        TabLayoutMediator(findViewById<TabLayout>(R.id.tab), findViewById<ViewPager2>(R.id.pager)){
+                tab, position ->
+            if(position ==0) {
+                tab.setIcon(R.drawable.home)
+            }else{
+                tab.setIcon(R.drawable.settings)
+            }
+
+        }.attach()
+
+        findViewById<TabLayout>(R.id.tab)
+    }
+}
