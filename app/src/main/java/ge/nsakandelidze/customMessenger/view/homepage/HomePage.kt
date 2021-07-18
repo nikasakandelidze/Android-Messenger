df7a7b1c7a5b5d975fe03b17d1c23af7e105f951
rezimeshvelashvili <rmesh16@freeuni.edu.ge>
2021-07-18 15:16:07 +0400

    homeactivity changed to home fragment

--- app/src/main/java/ge/nsakandelidze/customMessenger/view/homepage/HomePage.kt
+++ app/src/main/java/ge/nsakandelidze/customMessenger/view/homepage/HomePage.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ge.nsakandelidze.customMessenger.R
+import ge.nsakandelidze.customMessenger.presenter.homepage.HomePagePresenter
+import ge.nsakandelidze.customMessenger.view.dto.ConversationDto
+import ge.nsakandelidze.customMessenger.view.homepage.HomePageListAdapter
+import ge.nsakandelidze.customMessenger.view.homepage.IHomePageView
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -19,10 +26,14 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-class HomePage : Fragment(R.layout.home_page_activiy) {
+class HomePage : Fragment(R.layout.home_page_activiy), IHomePageView {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var conversationsListRecyclerView: RecyclerView
+    private lateinit var presenter: HomePagePresenter
+    private var listOfConversations: MutableList<ConversationDto> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,10 +51,22 @@ class HomePage : Fragment(R.layout.home_page_activiy) {
     ): View {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.home_page_activiy, container, false)
-
+        initViewComponents(view)
+        initState(view)
         return view
     }
 
+    private fun initViewComponents(view: View) {
+        conversationsListRecyclerView = view.findViewById<RecyclerView>(R.id.home_page_listing)
+        conversationsListRecyclerView.adapter = HomePageListAdapter(listOfConversations)
+        conversationsListRecyclerView.layoutManager =
+            LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+    }
+
+    private fun initState(view: View) {
+        this.presenter = HomePagePresenter(this)
+        this.presenter.fetchConversationForCurrentUser()
+    }
 
     companion object {
         /**
@@ -63,5 +86,20 @@ class HomePage : Fragment(R.layout.home_page_activiy) {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun notifyUser(message: String) {
+            Toast.makeText(activity, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun updateConversationsList(conversations: List<ConversationDto>) {
+            listOfConversations.clear()
+            listOfConversations.addAll(conversations)
+            conversationsListRecyclerView.adapter?.notifyDataSetChanged()
+    }
+
+    override fun AddNewConversationAndupdateConversationsList(conversation: ConversationDto) {
+            listOfConversations.add(conversation)
+            conversationsListRecyclerView.adapter?.notifyDataSetChanged()
     }
 }
